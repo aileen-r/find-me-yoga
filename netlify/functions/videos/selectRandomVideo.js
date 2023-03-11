0122f6b95f12957b7706385856b93200a9f4597d
Aileen Rae <aileenlrae@gmail.com>
2023-03-11 21:33:47 +0000

    Weight randomisation to videos with incomplete data

--- netlify/functions/videos/selectRandomVideo.js
+++ netlify/functions/videos/selectRandomVideo.js
@@ -1,6 +1,21 @@
-function selectRandomVideo(videos) {
+const NULL_COL_PRIORITY = ['duration', 'url', 'subscription', 'title', 'energy', 'style', 'instructor', 'bodyArea'];
+
+function random(videos) {
   const randomIdx = Math.round(Math.random() * videos.length);
+  if (!videos[randomIdx]) {
+    console.warn("Falling back to first video in list");
+  }
+  // console.log(videos.map(v => v.title));
   return videos[randomIdx] || videos[0]; // fallback just in case
+}
+
+function selectRandomVideo(videos) {
+  for (let col of NULL_COL_PRIORITY) {
+    if (videos.some(v => !v[col])) {
+      return random(videos.filter(v => !v[col]));
+    }
+  }
+  return random(videos);
 }
 
 export default selectRandomVideo;
