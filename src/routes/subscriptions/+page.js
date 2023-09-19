c110d35e246a0f253cad2c672b558dda40ec0107
Aileen Rae <aileenlrae@gmail.com>
2023-09-19 16:49:25 +0100

    Maybe I'm looking in the wrong place, try the load fn

--- src/routes/subscriptions/+page.js
+++ src/routes/subscriptions/+page.js
@@ -1,11 +1,15 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-  const res = await fetch(`/.netlify/functions/subscriptions`);
+  try {
+    const res = await fetch(`/.netlify/functions/subscriptions`);
 
-  /**
-   * Properties: name, free, thumbnail, url
-   */
-  const subscriptions = await res.json();
- 
-  return { subscriptions };
+    /**
+     * Properties: name, free, thumbnail, url
+     */
+    const subscriptions = await res.json();
+   
+    return { subscriptions };
+  } catch (err) {
+    console.error({err});
+  }
 }
--- src/stores/subscriptions.js
+++ src/stores/subscriptions.js
@@ -1,48 +1,48 @@
-// import { browser } from '$app/environment';
-import { writable } from 'svelte/store';
+// // import { browser } from '$app/environment';
+// import { writable } from 'svelte/store';
 
-/**
- * Properties: name, enabled
- */
-let persistedSubscriptions;
-// try {
-// 	if (browser) {
-// 		const itemFromStorage = localStorage.getItem('subscriptions');
-// 		console.log('itemFromStorage', itemFromStorage);
-// 	}
-// 	persistedSubscriptions = browser && JSON.parse(localStorage.getItem('subscriptions'));
-// } catch (err) {
-// 	console.error(err);
-// 	console.error('isBrowser:', browser);
-// 	console.log('browser variable', browser);
-// 	console.log('localStorage', localStorage);
-// 	throw 'is browser ' + browser;
+// /**
+//  * Properties: name, enabled
+//  */
+// let persistedSubscriptions;
+// // try {
+// // 	if (browser) {
+// // 		const itemFromStorage = localStorage.getItem('subscriptions');
+// // 		console.log('itemFromStorage', itemFromStorage);
+// // 	}
+// // 	persistedSubscriptions = browser && JSON.parse(localStorage.getItem('subscriptions'));
+// // } catch (err) {
+// // 	console.error(err);
+// // 	console.error('isBrowser:', browser);
+// // 	console.log('browser variable', browser);
+// // 	console.log('localStorage', localStorage);
+// // 	throw 'is browser ' + browser;
+// // }
+
+// function createSubscriptionsStore() {
+// 	const { subscribe, set: svelteSet, update } = writable(persistedSubscriptions || []);
+
+// 	return {
+// 		subscribe,
+// 		set: (subscriptions) => {
+// 			svelteSet(subscriptions);
+// 			// if (browser) {
+// 				localStorage?.setItem('subscriptions', JSON.stringify(subscriptions));
+// 			// }
+// 		},
+// 		add: (newSubsciptions) => {
+// 			let updatedSubscriptions = [];
+// 			update((subscriptions) => {
+// 				updatedSubscriptions = [...subscriptions, ...newSubsciptions];
+// 				return updatedSubscriptions;
+// 			});
+// 			// if (browser) {
+// 				localStorage?.setItem('subscriptions', JSON.stringify(updatedSubscriptions));
+// 			// }
+// 		}
+// 	};
 // }
 
-function createSubscriptionsStore() {
-	const { subscribe, set: svelteSet, update } = writable(persistedSubscriptions || []);
+// const subscriptionsStore = createSubscriptionsStore();
 
-	return {
-		subscribe,
-		set: (subscriptions) => {
-			svelteSet(subscriptions);
-			// if (browser) {
-				localStorage?.setItem('subscriptions', JSON.stringify(subscriptions));
-			// }
-		},
-		add: (newSubsciptions) => {
-			let updatedSubscriptions = [];
-			update((subscriptions) => {
-				updatedSubscriptions = [...subscriptions, ...newSubsciptions];
-				return updatedSubscriptions;
-			});
-			// if (browser) {
-				localStorage?.setItem('subscriptions', JSON.stringify(updatedSubscriptions));
-			// }
-		}
-	};
-}
-
-const subscriptionsStore = createSubscriptionsStore();
-
-export default subscriptionsStore;
+// export default subscriptionsStore;
