6f39e6f6ec2fdc887f6ce42ffa0e69d25a382ab0
Aileen Rae <aileenlrae@gmail.com>
2023-09-19 16:34:33 +0100

    Make the whole effing thing a try catch, where is the error???

--- src/stores/subscriptions.js
+++ src/stores/subscriptions.js
@@ -1,49 +1,55 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-/**
- * Properties: name, enabled
- */
-let persistedSubscriptions
+let subscriptionsStore = {};
+
 try {
-	if (browser) {
-		const itemFromStorage = localStorage.getItem('subscriptions');
-		console.log("itemFromStorage", itemFromStorage);
-	}
-  persistedSubscriptions = browser && JSON.parse(localStorage.getItem('subscriptions'));
-} catch (err) {
-	console.error(err);
-	console.error("isBrowser:", browser);
-	console.log("browser variable", browser);
-	console.log("localStorage", localStorage);
-	throw("is browser " + browser);
-}
-
-function createSubscriptionsStore() {
-	const { subscribe, set: svelteSet, update } = writable(persistedSubscriptions || []);
-
-	return {
-		subscribe,
-		set: (subscriptions) => {
-			svelteSet(subscriptions);
-			if (browser) {
-				localStorage.setItem('subscriptions', JSON.stringify(subscriptions));
-			}
-		},
-		add: (newSubsciptions) => {
-			let updatedSubscriptions = [];
-			update((subscriptions) => {
-				updatedSubscriptions = [...subscriptions, ...newSubsciptions];
-				return updatedSubscriptions;
-				
-			});
-			if (browser) {
-				localStorage.setItem('subscriptions', JSON.stringify(updatedSubscriptions));
-			}
+	/**
+	 * Properties: name, enabled
+	 */
+	let persistedSubscriptions;
+	try {
+		if (browser) {
+			const itemFromStorage = localStorage.getItem('subscriptions');
+			console.log('itemFromStorage', itemFromStorage);
 		}
-	};
-}
+		persistedSubscriptions = browser && JSON.parse(localStorage.getItem('subscriptions'));
+	} catch (err) {
+		console.error(err);
+		console.error('isBrowser:', browser);
+		console.log('browser variable', browser);
+		console.log('localStorage', localStorage);
+		throw 'is browser ' + browser;
+	}
 
-const subscriptionsStore = createSubscriptionsStore();
+	/* eslint-disable */
+	function createSubscriptionsStore() {
+		const { subscribe, set: svelteSet, update } = writable(persistedSubscriptions || []);
+
+		return {
+			subscribe,
+			set: (subscriptions) => {
+				svelteSet(subscriptions);
+				if (browser) {
+					localStorage.setItem('subscriptions', JSON.stringify(subscriptions));
+				}
+			},
+			add: (newSubsciptions) => {
+				let updatedSubscriptions = [];
+				update((subscriptions) => {
+					updatedSubscriptions = [...subscriptions, ...newSubsciptions];
+					return updatedSubscriptions;
+				});
+				if (browser) {
+					localStorage.setItem('subscriptions', JSON.stringify(updatedSubscriptions));
+				}
+			}
+		};
+	}
+
+	const subscriptionsStore = createSubscriptionsStore();
+} catch (err) {
+	console.error({err});
+}
 
 export default subscriptionsStore;
