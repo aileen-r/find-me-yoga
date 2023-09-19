cc615918322d012397228101a4f79703d8bb571a
Aileen Rae <aileenlrae@gmail.com>
2023-09-19 20:34:42 +0100

    Get thumbnail link markup correct before nesting a secondary button

--- src/components/landing/VideoResult.svelte
+++ src/components/landing/VideoResult.svelte
@@ -15,9 +15,14 @@
 </script>
 
 <article class="text-center">
-	<a class="block focus-visible:outline-none focus-visible:ring focus-visible:ring-zinc-400 focus-visible:ring-offset-4 focus-visible:rounded" href={video.url} target="_blank" rel="noopener noreferrer nofollow"
-		><h2 class="underline-hover-inverted w-max text-2xl font-bold mx-auto mb-3 pb-2">
-			{video.title}
+	<div class="card">
+		<h2 class="underline-hover-inverted w-max text-2xl font-bold mx-auto mb-3 pb-2">
+			<a
+				class="card-primary-action"
+				href={video.url}
+				target="_blank"
+				rel="noopener noreferrer nofollow">{video.title}</a
+			>
 		</h2>
 		<VideoThumbnail
 			thumbnail={video.thumbnail}
@@ -25,34 +30,31 @@
 			subscription={video.subscription}
 			duration={video.duration}
 		/>
-	</a>
+	</div>
 
 	{#if others.length}
 		<h3 class="text-xl mt-8 mb-3 text-left">Or, you could try...</h3>
 		<div class="flex gap-2">
 			{#each others as otherVideo}
-				<div class="w-1/3">
-					<a
-						class="block focus-visible:outline-none focus-visible:ring focus-visible:ring-zinc-400 focus-visible:ring-offset-1 focus-visible:rounded-sm"
-						href={otherVideo.url}
-						target="_blank"
-						rel="noopener noreferrer nofollow">
-						<VideoThumbnail
-							thumbnail={otherVideo.thumbnail}
-							title={otherVideo.title}
-							subscription={otherVideo.subscription}
-							duration={otherVideo.duration}
-							size="small"
-						/>
-						<h4 class="underline hover:no-underline">{otherVideo.title}</h4></a
-					>
+				<div class="card w-1/3">
+					<VideoThumbnail
+						thumbnail={otherVideo.thumbnail}
+						title={otherVideo.title}
+						subscription={otherVideo.subscription}
+						duration={otherVideo.duration}
+						size="small"
+					/>
+					<h4 class="underline hover:no-underline">
+						<a
+							class="card-primary-action"
+							href={otherVideo.url}
+							target="_blank"
+							rel="noopener noreferrer nofollow">{otherVideo.title}</a
+						>
+					</h4>
 				</div>
 			{/each}
 		</div>
 	{/if}
-	<button
-		class="btn"
-		on:click={backToStart}
-		type="button">Back to start</button
-	>
+	<button class="btn" on:click={backToStart} type="button">Back to start</button>
 </article>
