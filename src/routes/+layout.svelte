34304f5932cc93da46784d48c918000acb7a029e
Aileen Rae <aileenlrae@gmail.com>
2023-10-06 19:22:12 +0100

    Improve page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,27 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import Navbar from '../components/global/Navbar.svelte';
 	import Footer from '../components/global/Footer.svelte';
 
 	import '../reset.css';
 	import '../main.css';
+
+	export let data;
+	console.log(data);
 </script>
 
 <div class="flex flex-col min-h-full">
 	<Navbar />
 
-	<main class="flex flex-col mt-6 mx-auto flex-1 max-w-4xl px-4 py-2.5 w-full relative">
-		<slot />
-	</main>
+	{#key data.url.pathname}
+		<main
+			class="flex flex-col mt-6 mx-auto flex-1 max-w-4xl px-4 py-2.5 w-full relative"
+			in:fade={{duration: 300, delay: 301}}
+			out:fade={{duration: 300}}
+		>
+			<slot />
+		</main>
+	{/key}
 
 	<Footer />
 </div>
@@ -19,10 +29,10 @@
 <style lang="scss">
 	// reusible classes
 	:global(.underline-hover) {
-		background: linear-gradient(0deg, theme('colors.zinc.400'), theme('colors.zinc.400')) no-repeat right
-			bottom / 0 var(--bg-h);
+		background: linear-gradient(0deg, theme('colors.zinc.400'), theme('colors.zinc.400')) no-repeat
+			right bottom / 0 var(--bg-h);
 		transition: background-size 350ms;
-  	--bg-h: 0.15em;
+		--bg-h: 0.15em;
 
 		&:hover {
 			background-size: 100% var(--bg-h);
@@ -31,10 +41,10 @@
 	}
 
 	:global(.underline-hover-inverted) {
-		background: linear-gradient(0deg, theme('colors.zinc.400'), theme('colors.zinc.400')) no-repeat left
-			bottom / 100% var(--bg-h);
+		background: linear-gradient(0deg, theme('colors.zinc.400'), theme('colors.zinc.400')) no-repeat
+			left bottom / 100% var(--bg-h);
 		transition: background-size 350ms;
-  	--bg-h: 0.15em;
+		--bg-h: 0.15em;
 
 		&:hover {
 			background-size: 0 var(--bg-h);
@@ -44,6 +54,5 @@
 		&:focus-visible {
 			background: transparent;
 		}
-
 	}
 </style>
